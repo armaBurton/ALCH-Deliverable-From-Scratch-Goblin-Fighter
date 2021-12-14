63f2262962746a1570306602f5eb9644dd165554
armaBurton <armanarma@armaburton.com>
2021-12-13 17:08:22 -0800

    dead goblins become transparant and stay on the battlefield, combat actions are now tracked in the scrolling combat text panel

--- script/doesDamage.js
+++ script/doesDamage.js
@@ -1,8 +1,22 @@
+import { scrollingCombatText, sct } from "./script.js";
+
 export function doesDamage(playerHP){
     let hitRoll = doesGoblinHit();
     console.log(hitRoll);
 
-    hitRoll ? playerHP-- : playerHP;
+    const playerTakesDamage = document.createElement(`p`);
+    playerTakesDamage.classList.add(`strawberry`);
+    if (hitRoll){
+        playerTakesDamage.textContent = `You have been damaged by a goblin.`;
+        scrollingCombatText.push(playerTakesDamage);
+        playerHP--;
+    } else {
+        playerTakesDamage.textContent = `A goblin missed hitting you.`;
+        scrollingCombatText.push(playerTakesDamage);
+    }
+    
+    
+    sct.prepend(playerTakesDamage);
     
     return playerHP;
 }
